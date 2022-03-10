896064465d7f02a334c765b3048f273782fd41b1
Anna Hlystova <hlystova_aa@magnit.ru>
2022-03-10 22:59:06 +0300

    Модуль 1 Задача 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,14 @@
 function factorial(n) {
-  // ваш код...
+	let result = 1;
+
+		if ( n > 1 ) {
+			for (let i=1; i<=n; i++) {
+				result *= i;
+			}
+		} else if ( !(typeof n == "number") ) { // если ввели не число
+			alert('В качестве аргумента необходимо число :)');
+			return;
+		}
+		
+		return result;
 }
