d2d9790ef96fcdc4ea1ce4d9d87334bd1b1445a4
Anna Hlystova <hlystova_aa@magnit.ru>
2022-03-25 10:20:32 +0300

    3 module 3 tasks

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,21 @@
 function highlight(table) {
-  // ваш код...
+  for (let i=0; i<table.tBodies[0].rows.length; i++) {
+		
+		let tr = table.tBodies[0].rows[i],
+			attr = tr.cells[3].getAttribute('data-available'),
+			gender = tr.cells[2].textContent,
+			age = +tr.cells[1].textContent;
+    
+    if (attr === 'true') {
+      tr.classList.add('available');
+    } else if (attr === 'false') {
+      tr.classList.add('unavailable');
+    } else {
+      tr.hidden = true
+    }
+
+		gender === 'm' ? tr.classList.add('male') : tr.classList.add('female');
+
+		age < 18 ? tr.style.textDecoration = 'line-through' : '';
+	}
 }
